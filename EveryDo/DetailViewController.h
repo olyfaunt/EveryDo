c12b39d592c3ff6c4ff5f2b0ff372d34df48e28c
Audrey Jun <audrey.jun@gmail.com>
2014-10-22 11:20:52 -0700

    Initial Commit

---
+++ EveryDo/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  EveryDo
+//
+//  Created by Audrey Jun on 2014-10-22.
+//  Copyright (c) 2014 audreyjun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@end
+
---
+++ EveryDo/MasterViewController.h
@@ -0,0 +1,15 @@
+//
+//  MasterViewController.h
+//  EveryDo
+//
+//  Created by Audrey Jun on 2014-10-22.
+//  Copyright (c) 2014 audreyjun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+
+@end
+
